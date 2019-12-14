0f143176d7703e0cdd93a627f684d961f0d998f6
sisizanohito <sisizanohito@gmail.com>
2019-12-15 03:43:33 +0900

    とりあえずプロジェクトを用意

---
+++ phoenix_point_JP/extract_text/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extract_text
+{
+    class Program
+    {
+        const string WORKDIR = "./Resources";
+        static void Main(string[] args)
+        {
+            IEnumerable<string> files =
+            System.IO.Directory.EnumerateFiles(
+            WORKDIR, "*", System.IO.SearchOption.AllDirectories);
+
+            //ファイルを列挙する
+            foreach (string f in files)
+            {
+                Console.WriteLine(f);
+            }
+
+#if DEBUG
+            Console.ReadKey();
+            #endif
+        }
+    }
+}
---
+++ phoenix_point_JP/phoenix_point_JP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phoenix_point_JP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        #if DEBUG
+            Console.ReadKey();
+        #endif
+
+        }
+    }
+}
