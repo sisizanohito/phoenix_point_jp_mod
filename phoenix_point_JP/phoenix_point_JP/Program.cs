4f2ef92590b166409e64845136885c0646e8eccd
sisizanohito <sisizanohito@gmail.com>
2020-01-19 18:58:51 +0900

    設定ファイルを追加

--- phoenix_point_JP/phoenix_point_JP/Program.cs
+++ phoenix_point_JP/phoenix_point_JP/Program.cs
@@ -3,17 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace phoenix_point_JP
 {
     class Program
     {
+        const string WORKDIR = "./Resources/";
         static void Main(string[] args)
         {
+            IEnumerable<string> files =
+            System.IO.Directory.EnumerateFiles(
+            WORKDIR, "*.tsv", System.IO.SearchOption.AllDirectories);
+
+            //ファイルを列挙する
+            foreach (string f in files)
+            {
+                Console.WriteLine(f);
+                ReadTSV(f);
+            }
         #if DEBUG
             Console.ReadKey();
         #endif
 
+        }
+
+        static void ReadTSV(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName, Encoding.UTF8);
+            try
+            {
+                while (sr.EndOfStream == false)
+                {
+                    string line = sr.ReadLine();
+                    //string[] fields = line.Split(',');
+                    string[] fields = line.Split('\t'); //TSVファイルの場合
+
+                    for (int i = 0; i < fields.Length; i++)
+                    {
+                        Console.Write(fields[i] + "  ");
+                    }
+                    Console.Write("------\r\n");
+
+                }
+            }
+            finally
+            {
+                sr.Close();
+            }
+            
         }
     }
 }
---
+++ phoenix_point_JP/phoenix_point_JP/SettingInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phoenix_point_JP
+{
+    public class SettingInfo
+    {
+        public string TargetDirectory { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string[] keys { get; set; }
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileString(
+    string lpApplicationName,
+    string lpKeyName,
+    string lpDefault,
+    StringBuilder lpReturnedstring,
+    int nSize,
+    string lpFileName);
+
+        public string GetIniValue(string path, string section, string key)
+        {
+            StringBuilder sb = new StringBuilder(256);
+            GetPrivateProfileString(section, key, string.Empty, sb, sb.Capacity, path);
+            return sb.ToString();
+        }
+    }
+
+}
