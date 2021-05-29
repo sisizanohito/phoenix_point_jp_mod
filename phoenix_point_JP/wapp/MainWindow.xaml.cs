fe43aae4c653f196cfde9570a01281e7469a0471
sisizanohito <sisizanohito@gmail.com>
2021-05-29 15:28:40 +0900

    更新対応

--- phoenix_point_JP/wapp/MainWindow.xaml.cs
+++ phoenix_point_JP/wapp/MainWindow.xaml.cs
@@ -74,11 +74,11 @@ namespace wapp
             pbat.WaitForExit();
             pbat.Close();
 
-            File.Copy(@"./REPACK/sharedassets0_00001.-203", subpath + @"sharedassets0_00001.-203", true);
-            File.Copy(@"./REPACK/sharedassets0_00002.-203", subpath + @"sharedassets0_00002.-203", true);
-            File.Copy(@"./REPACK/sharedassets0_00003.-203", subpath + @"sharedassets0_00003.-203", true);
-            File.Copy(@"./REPACK/sharedassets0_00004.-203", subpath + @"sharedassets0_00004.-203", true);
-            File.Copy(@"./REPACK/sharedassets0_00005.-203", subpath + @"sharedassets0_00005.-203", true);
+            File.Copy(@"./REPACK/sharedassets0_00001.-212", subpath + @"sharedassets0_00001.-212", true);
+            File.Copy(@"./REPACK/sharedassets0_00002.-212", subpath + @"sharedassets0_00002.-212", true);
+            File.Copy(@"./REPACK/sharedassets0_00003.-212", subpath + @"sharedassets0_00003.-212", true);
+            File.Copy(@"./REPACK/sharedassets0_00004.-212", subpath + @"sharedassets0_00004.-212", true);
+            File.Copy(@"./REPACK/sharedassets0_00006.-212", subpath + @"sharedassets0_00006.-212", true);
 
             Process punityex = new Process();
             punityex.StartInfo.FileName = @".\UnityEX.exe";
